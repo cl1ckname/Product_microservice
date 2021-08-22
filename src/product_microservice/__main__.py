01c4589963ab3e1dd53678a4ac492f576df6787e
cl1ckname <20002mc@gmail.com>
2021-08-22 11:17:36 +0300

    Get product by id func

--- src/product_microservice/__main__.py
+++ src/product_microservice/__main__.py
@@ -1,6 +1,6 @@
 from aiohttp import web
 from pymongo import MongoClient
-from .mongo_services import insert_product, get_products
+from .mongo_services import get_by_id, insert_product, get_products
 
 print('Trying connect to MongoDB....')
 client = MongoClient('localhost:27017')
@@ -16,18 +16,31 @@ async def add_product(request: web.Request):
             params = data.get('params', {})
             insert_product(products_db, {'name': name, 'describe': describe, 'params': params})
             return web.json_response({'status': 'access'}, status=200)
-    except ValueError:
+    except (ValueError, KeyError):
         return web.json_response({'status': 'failed', 'message': 'Bad request'}, status=400)
 
 async def get_product(request: web.Request):
     try:
-        return web.json_response(get_products(products_db), status=200)
+        return web.json_response({'status': 'success', 'data': get_products(products_db)}, status=200)
     except ValueError:
         return web.json_response({'status': 'failed', 'message': 'Bad request'}, status=400)
+
+async def get_product_by_id(request: web.Request):
+    try:
+        data = await request.json()
+        _id = data['id']
+        product = get_by_id(_id)
+        if not product:
+            return web.json_response({'status': 'failed', 'mesage': 'There is no product with this id'}, status=404)
+        return web.json_response({'status': 'success', 'data': product}, status=200)
+    except ZeroDivisionError:
+        return web.json_response({'status': 'failed', 'message': 'Bad request'}, status=400)
+
 
 
 app = web.Application()
 app.router.add_route('POST', '/', add_product, expect_handler = web.Request.json)
 app.router.add_route('GET', '/all', get_product)
+app.router.add_route('GET', '/by_id', get_product_by_id)
 
 web.run_app(app)
--- src/product_microservice/mongo_services.py
+++ src/product_microservice/mongo_services.py
@@ -1,13 +1,23 @@
+""" Module with various methods for interacting with MongoDb """
+
+from bson.objectid import ObjectId
 from pymongo.database import Database
 
 
-def insert_product(db: Database, data: dict):
+def insert_product(db: Database, data: dict) -> None:
     products = db['products']
     products.insert_one(data)
 
-def get_products(db: Database):
+def get_products(db: Database) -> list:
     data = []
     for product in db['products'].find({'name': 'iphone'}):
         product['_id'] = str(product['_id'])
         data.append(product)
     return data
+
+def get_by_id(db: Database, id:str) -> dict:
+    product = db['products'].find_one({'_id': ObjectId(id)})
+    if product:
+        product['_id'] = str(product['_id'])
+        return product
+    return None
