b1d797653887e535db25fa19c994368248e68244
cl1ckname <20002mc@gmail.com>
2021-08-22 18:44:34 +0300

    Add sorting and search by params

--- src/product_microservice/__main__.py
+++ src/product_microservice/__main__.py
@@ -1,46 +1,70 @@
 from aiohttp import web
 from pymongo import MongoClient
-from .mongo_services import get_by_id, insert_product, get_products
+from .mongo_services import get_by_id, insert_product, get_all, get_products
 
 print('Trying connect to MongoDB....')
 client = MongoClient('localhost:27017')
 print('Connection success!')
 products_db = client['products']
 
+
+def safe_route(func):
+    ''' Catches errors during route execution, in case of an error, returns a response json '''
+    async def wrapper(*args, **kwargs) -> web.Response:
+        try:
+            return await func(*args, **kwargs)
+        except (ValueError, KeyError):
+            return web.json_response({'status': 'failed', 'message': 'Bad request'}, status=400)
+    return wrapper
+
+
+@safe_route
 async def add_product(request: web.Request):
-    try:
-        if request.content_type == 'application/json':
-            data:dict = await request.json()
-            name = data['name']
-            describe = data['describe']
-            params = data.get('params', {})
-            insert_product(products_db, {'name': name, 'describe': describe, 'params': params})
-            return web.json_response({'status': 'access'}, status=200)
-    except (ValueError, KeyError):
-        return web.json_response({'status': 'failed', 'message': 'Bad request'}, status=400)
+    ''' Add product to collection '''
+    data: dict = await request.json()
+    name = data['name']
+    describe = data['describe']
+    params = data.get('params', [])
+    insert_product(
+        products_db, {'name': name, 'describe': describe, 'params': params})
+    return web.json_response({'status': 'access'}, status=200)
 
-async def get_product(request: web.Request):
-    try:
-        return web.json_response({'status': 'success', 'data': get_products(products_db)}, status=200)
-    except ValueError:
-        return web.json_response({'status': 'failed', 'message': 'Bad request'}, status=400)
 
+@safe_route
+async def get_all_products(request: web.Request):
+    ''' Return all products '''
+    return web.json_response({'status': 'success', 'data': get_all(products_db)}, status=200)
+
+
+@safe_route
 async def get_product_by_id(request: web.Request):
-    try:
-        data = await request.json()
-        _id = data['id']
-        product = get_by_id(_id)
-        if not product:
-            return web.json_response({'status': 'failed', 'mesage': 'There is no product with this id'}, status=404)
-        return web.json_response({'status': 'success', 'data': product}, status=200)
-    except ZeroDivisionError:
-        return web.json_response({'status': 'failed', 'message': 'Bad request'}, status=400)
+    ''' Returns product with the specified id '''
+    data = await request.json()
+    _id = data['id']
+    product = get_by_id(products_db, _id)
+    if not product:
+        return web.json_response({'status': 'failed', 'mesage': 'There is no product with such params'}, status=404)
+    return web.json_response({'status': 'success', 'data': product}, status=200)
 
 
+@safe_route
+async def get_product_by_param(request: web.Request):
+    ''' Returns products with the specified parameters and with the specified name '''
+    data = await request.json()
+    params = data['params']
+    sort_by = data.get('sort_by', None)
+    products = get_products(products_db, params, sort=sort_by)
+    if not products:
+        return web.json_response({'status': 'failed', 'mesage': 'There is no product with this id'}, status=404)
+    return web.json_response({'status': 'success', 'data': products}, status=200)
 
 app = web.Application()
-app.router.add_route('POST', '/', add_product, expect_handler = web.Request.json)
-app.router.add_route('GET', '/all', get_product)
-app.router.add_route('GET', '/by_id', get_product_by_id)
+app.router.add_route('POST', '/add', add_product,
+                     expect_handler=web.Request.json)
+app.router.add_route('GET', '/all', get_all_products)
+app.router.add_route('POST', '/by_id', get_product_by_id,
+                     expect_handler=web.Request.json)
+app.router.add_route('POST', '/by_param', get_product_by_param,
+                     expect_handler=web.Request.json)
 
 web.run_app(app)
--- src/product_microservice/mongo_services.py
+++ src/product_microservice/mongo_services.py
@@ -5,19 +5,47 @@ from pymongo.database import Database
 
 
 def insert_product(db: Database, data: dict) -> None:
+    ''' Creates a document in the Products collection. '''
     products = db['products']
     products.insert_one(data)
 
-def get_products(db: Database) -> list:
+def get_all(db: Database) -> list:
+    ''' Returns all documents in products collection'''
     data = []
-    for product in db['products'].find({'name': 'iphone'}):
+    products = db['products'].find({})
+    for product in products:
         product['_id'] = str(product['_id'])
         data.append(product)
+
     return data
 
 def get_by_id(db: Database, id:str) -> dict:
+    ''' Returns document with specified id '''
     product = db['products'].find_one({'_id': ObjectId(id)})
     if product:
         product['_id'] = str(product['_id'])
         return product
     return None
+
+def get_products(db: Database, params: list, sort:str = None, name:str = None):
+    ''' Returns document with specified id '''
+    data = []
+    find_params = {}
+    if params:
+        find_params['params'] = {'$all':params}
+    if sort:
+        find_params['params.'+sort] = {'$exists': True}
+    if name:
+        find_params['name'] = name
+    
+    products = db['products'].find(find_params)
+    if sort:
+        if sort == "name":
+            products = products.sort(sort)
+        else:
+            products = products.sort('params.'+sort)
+
+    for product in products:
+        product['_id'] = str(product['_id'])
+        data.append(product)
+    return data
